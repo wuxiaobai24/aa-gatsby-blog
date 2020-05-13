331b476cad410c751424a4722c5ab4accdcb8c35
wuxiaobai24 <wuxiaobai24@foxmail.com>
2020-05-13 09:57:38 +0800

    Add tags and categories page

---
+++ src/pages/tags.jsx
@@ -0,0 +1,37 @@
+import React from "react"
+import { Link, graphql } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import limax from "limax"
+
+export default ({ data }) => {
+  const slugFunc = tag => limax(tag, { tone: false })
+  const tags = data.allMarkdownRemark.tags.map(({ fieldValue, totalCount }) => {
+    const slug = slugFunc(fieldValue)
+    return (
+      <div className="block-inline m-4 bg-blue-300" key={slug}>
+        <Link to={`/tags/${slug}`} className="p-2">
+          {totalCount} - {fieldValue}
+        </Link>
+      </div>
+    )
+  })
+  return (
+    <Layout>
+      <SEO title="Tags" />
+			<div className="p-8 bg-blue-100">{tags}</div>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  {
+    allMarkdownRemark {
+      tags: group(field: frontmatter___tags) {
+				fieldValue
+				totalCount
+			}
+    }
+  }
+`
