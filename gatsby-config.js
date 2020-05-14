24d7fe0df43ee5c715f81e69152e0022b0a512ea
wuxiaobai24 <wuxiaobai24@foxmail.com>
2020-05-14 10:42:13 +0800

    add netlify cms

--- gatsby-config.js
+++ gatsby-config.js
@@ -87,7 +87,7 @@ module.exports = {
         ],
       },
     },
-
+    `gatsby-plugin-netlify-cms`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
