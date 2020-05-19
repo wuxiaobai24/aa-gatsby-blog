19a20e08e20c701c0f61256852d0bcbaac33a7ec
wuxiaobai24 <wuxiaobai24@foxmail.com>
2020-05-19 17:21:11 +0800

    fix bug

--- src/components/layout.js
+++ src/components/layout.js
@@ -22,7 +22,7 @@ const Layout = ({ children }) => {
     { url: "/", name: "Home" },
     { url: "/archive", name: "Archive" },
     { url: "/tags/", name: "Tags" },
-    { url: "/bookshelf/", name: "Books"}
+    { url: "/bookshelf/", name: "Books"},
     { url: "/about", name: "About" },
   ]
 
