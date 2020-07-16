9aaa32298fddbe2db7e2ab545b0c811a479e2801
wuxiaobai24 <wuxiaobai24@foxmail.com>
2020-07-16 13:48:32 +0800

    fix Taglist

--- src/pages/tags.jsx
+++ src/pages/tags.jsx
@@ -3,22 +3,26 @@ import { Link, graphql } from "gatsby"
 
 import Layout from "../components/page-layout"
 import SEO from "../components/seo"
-import limax from "limax"
+import { TagList } from "../components/tags"
 
 export default ({ data }) => {
-  const slugFunc = tag => limax(tag, { tone: false })
-  const tags = data.allMdx.tags.map(({ fieldValue, totalCount }) => {
-    const slug = slugFunc(fieldValue)
-    return (
-      <Link to={`/tags/${slug}`} className="post-header-tagitem" key={slug}>
-        {fieldValue}
-      </Link>
-    )
-  })
+  // const slugFunc = tag => limax(tag, { tone: false })
+  // const tags = data.allMdx.tags.map(({ fieldValue, totalCount }) => {
+  //   const slug = slugFunc(fieldValue)
+  //   return (
+  //     <Link to={`/tags/${slug}`} className="post-header-tagitem" key={slug}>
+  //       {fieldValue}
+  //     </Link>
+  //   )
+  // })
+  const tags = data.allMdx.tags.map(({ fieldValue }) => fieldValue)
   return (
     <Layout>
       <SEO title="Tags" />
-      <div className="post-header-tags">{tags}</div>
+      {/* <div className="post-header-tags">{tags}</div> */}
+      <div className="box">
+        <TagList tags={tags} />
+      </div>
     </Layout>
   )
 }
